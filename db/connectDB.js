b8fada7ceaafccba3d0c906d5191f3e7496bbb6e
LinearLaw <17817363770@163.com>
2018-06-15 11:14:12 +0800

    update db

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,19 +1,7 @@
-/*exports.connectDB = (mongoose) => { 
 
-   let db = mongoose.connect("mongodb://127.0.0.1:27017/xh_meeting",(err)=>{
-      if(err){
-        console.log("数据库连接失败！");
-      }else{
-         console.log("数据库连接成功！");
-      }
-   });
-   
-};
-
-*/
 exports.connectDB = (mongoose) => { 
 
-   let db = mongoose.connect("mongodb:localhost:27017",(err)=>{
+   let db = mongoose.connect("mongodb://localhost:27017",(err)=>{
       if(err){
         console.log("数据库连接失败！");
       }else{
