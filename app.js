26453cd9c7be67287cdb8cc8164dc495d78279ff
LinearLaw <17817363770@163.com>
2018-06-19 11:50:45 +0800

    update

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ global.io = require("socket.io")(http);
 
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+
+//multer , 上传文件的中间件
 const multer = require('multer');
+//cors，跨域资源的中间件
 const cors = require('cors');
+
 global.session = require('express-session');
 const cookieParser = require('cookie-parser');
 global.request = require('request');
@@ -16,8 +20,8 @@ global.path = require('path');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
-app.use(multer());
 
+app.use(multer());
 app.use(cors());
 
 //连接数据库
@@ -41,5 +45,5 @@ io.on("connection",function(socket){
 
 
 http.listen(9000,()=>{
-    console.log("端口号9000，服务已就绪。")
+    console.log("端口号9000，服务已就绪。");
 })
