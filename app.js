7a6dd7bea2f25f4854717c1d2ccb90906ebacdd1
LinearLaw <17817363770@163.com>
2018-06-19 15:46:44 +0800

    update router.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 const http = require("http").Server(app);
 global.io = require("socket.io")(http);
 
@@ -11,12 +12,25 @@ const multer = require('multer');
 //cors，跨域资源的中间件
 const cors = require('cors');
 
+const router = require("./router/router.js");
+
 global.session = require('express-session');
 const cookieParser = require('cookie-parser');
 global.request = require('request');
 global.fs = require('fs');
 global.path = require('path');
 
+
+//路由过滤
+app.use((req, res, next)=>{
+  if(req.path=="/html/chatRoom.html"){
+    res.send({ msg:"Error" });
+  }else{
+    next();
+  };
+});
+
+//中间件挂载
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
@@ -24,17 +38,11 @@ app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
 app.use(multer());
 app.use(cors());
 
-//连接数据库
-// let connectDB = require("./db/connectDB.js");
-// connectDB.connectDB(mongoose);
+//挂载路由模块
+app.use('/', router);
 
-// //创建表
-// let userData = require("./db/userData.js");
-// userData.userData(mongoose);
-
-//推送
+//socket服务推送
 let apiSocket = require("./api/apiSocket.js");
-
 io.on("connection",function(socket){
     apiSocket.apiSocket(socket);
     let count = io.eio.clientsCount;
@@ -43,7 +51,7 @@ io.on("connection",function(socket){
     })
 });
 
-
+//监听服务
 http.listen(9000,()=>{
     console.log("端口号9000，服务已就绪。");
 })
---
+++ router/router.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+let router = express.Router();
+
+//Router
+router.get("/room/:roomid",function(req,res){
+  var roomID = req.params.roomid;
+  var p = path.join(__dirname, "../public/html/chatRoom.html")
+  res.sendFile(p);
+})
+
+
+module.exports = router;
